7bd8aa50b0b23384750b56a7f75fa4ffe3b0bc01
BerkaySivri35 <1804107017@std.idu.edu.tr>
2022-08-14 17:53:55 +0300

    Initial commit

--- moviebrowser/src/components/Navbar.js
+++ moviebrowser/src/components/Navbar.js
@@ -17,7 +17,7 @@ const Navbar = ({ searchText, setSearchText }) => {
     return (
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
             <div className="container-fluid">
-                <Link className="navbar-brand" to="/home"><svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" className="bi bi-camera-reels-fill" viewBox="0 0 16 16">
+                <Link className="navbar-brand" to="/moviebrowser"><svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" className="bi bi-camera-reels-fill" viewBox="0 0 16 16">
                     <path d="M6 3a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
                     <path d="M9 6a3 3 0 1 1 0-6 3 3 0 0 1 0 6z" />
                     <path d="M9 6h.5a2 2 0 0 1 1.983 1.738l3.11-1.382A1 1 0 0 1 16 7.269v7.462a1 1 0 0 1-1.406.913l-3.111-1.382A2 2 0 0 1 9.5 16H2a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h7z" />
@@ -28,7 +28,7 @@ const Navbar = ({ searchText, setSearchText }) => {
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                         <li className="nav-item">
-                            <Link className="nav-link active" aria-current="page" to="/home">Home</Link>
+                            <Link className="nav-link active" aria-current="page" to="/moviebrowser">Home</Link>
                         </li>
 
                         <li className="nav-item d-block d-lg-none">
